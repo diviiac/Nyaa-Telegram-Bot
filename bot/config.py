791e095d78e9da46b18e8c55d70b166c433942d8
diviiac <93221743+diviiac@users.noreply.github.com>
2021-11-02 12:10:58 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,4 +1,4 @@
 class Config(object):
-    API_ID = int()
-    API_HASH = ""
-    BOT_TOKEN = ""
+    API_ID = int(8978543)
+    API_HASH = "44519bc2f82cfd5366dba966c17524da"
+    BOT_TOKEN = "2089390121:AAFGyR4SuCVRKjBMyWJJYPhwimOdqkRB_Gw"
